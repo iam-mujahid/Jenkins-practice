e4c2d0453cea939a0675008c4200426b39fa8c22
iam-mujahid <mujahidmohammed.13.11@gmail.com>
2026-09-23 10:24:45 +0530

    lab 05 test with intentional error to check the pipeline

--- Jenkins-lab-05/test.js
+++ Jenkins-lab-05/test.js
@@ -2,8 +2,7 @@ const { add } = require('./app');
 
 console.log('Running tests...');
 
-if (add(2, 3) !== 5) {
-    console.error('❌ Test failed');
+if (add(2, 3) !== 666666   console.error('❌ Test failed');
     process.exit(1);
 }
 
