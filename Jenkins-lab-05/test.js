f57977c8d167465a05fea852f5757bb757b4c4c2
iam-mujahid <mujahidmohammed.13.11@gmail.com>
2026-09-23 10:32:26 +0530

    lab 05 intentional error part 2 check

--- Jenkins-lab-05/test.js
+++ Jenkins-lab-05/test.js
@@ -2,7 +2,8 @@ const { add } = require('./app');
 
 console.log('Running tests...');
 
-if (add(2, 3) !== 666666   console.error('❌ Test failed');
+if (add(2, 3) !== 6){
+    console.error('❌ Test failed');
     process.exit(1);
 }
 
